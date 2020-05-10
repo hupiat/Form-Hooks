b4b96c1661a21e04b88747a6086c29336280cc35
Hugo Piat <h.piat@outlook.fr>
2020-05-10 02:43:17 +0200

    Adding declaration file

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { useFormState } from "./src/State";
+import { useFormValidation } from "./src/Validation";
+import { useFormSelect } from "./src/Select";
+export { useFormState, useFormValidation, useFormSelect };
