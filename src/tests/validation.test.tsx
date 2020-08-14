0c0b26d0763b1b644050388efd4edb2a80962ff8
Hugo Piat <h.piat@outlook.fr>
2020-08-14 04:41:50 +0200

    Fixing tests

--- src/tests/validation.test.tsx
+++ src/tests/validation.test.tsx
@@ -31,95 +31,101 @@ const mockAltSchema: FormValidationSchema<Flower> = {
   name: Joi.string().min(5),
 };
 
-let container: HTMLDivElement | null;
+describe("formook", () => {
+  let container: HTMLDivElement | null;
 
-beforeAll(() => {
-  EnvironmentHandler().setEnv("test");
-});
+  beforeAll(() => {
+    EnvironmentHandler().setEnv("test");
+  });
 
-beforeEach(() => {
-  switchHighLevelValidation("yup");
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+  beforeEach(() => {
+    switchHighLevelValidation("yup");
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
-afterEach(() => {
-  document.body.removeChild(container as HTMLDivElement);
-  container = null;
-});
+  afterEach(() => {
+    document.body.removeChild(container as HTMLDivElement);
+    container = null;
+  });
 
-const waitForResult = async (
-  rendered: RenderHookResult<any, FormValidation<Flower>>
-) => {
-  await rendered.waitForNextUpdate();
-  await rendered.waitForNextUpdate();
-};
-
-it("should be able to validate without errors", async () => {
-  const expectation = async (
-    rendered: RenderHookResult<unknown, FormValidation<Flower>>
+  const waitForResult = async (
+    rendered: RenderHookResult<any, FormValidation<Flower>>
   ) => {
-    await rendered.wait(() => rendered.result.current.canValidate);
-    expect(rendered.result.current.canValidate).toBeTruthy();
-    expect(rendered.result.current.errors).toHaveLength(0);
+    await rendered.waitForNextUpdate();
+    await rendered.waitForNextUpdate();
   };
 
-  // With a yup condition
-  let rendered = renderHook(() => useFormValidation(mockSchema, mockFlower));
-  expectation(rendered);
+  it("should be able to validate without errors", async () => {
+    const expectation = async (
+      rendered: RenderHookResult<unknown, FormValidation<Flower>>
+    ) => {
+      await rendered.wait(() => rendered.result.current.canValidate);
+      expect(rendered.result.current.canValidate).toBeTruthy();
+      expect(rendered.result.current.errors).toHaveLength(0);
+    };
 
-  // With a joi condition
-  switchHighLevelValidation("joi");
-  rendered.rerender({
-    schema: mockAltSchema,
-    object: JSON.parse(JSON.stringify(mockFlower)),
+    // With a yup condition
+    let rendered = renderHook(() => useFormValidation(mockSchema, mockFlower));
+    expectation(rendered);
+
+    // With a joi condition
+    switchHighLevelValidation("joi");
+    rendered.rerender({
+      schema: mockAltSchema,
+      object: JSON.parse(JSON.stringify(mockFlower)),
+    });
+    expectation(rendered);
   });
-  expectation(rendered);
-});
 
-it("should be blocked by yup", async () => {
-  const rendered = renderHook(() =>
-    useFormValidation(mockSchema, {
-      ...mockFlower,
-      name: "",
-    })
-  );
-  await waitForResult(rendered);
+  it("should be blocked by yup", async () => {
+    const rendered = renderHook(() =>
+      useFormValidation(mockSchema, {
+        ...mockFlower,
+        name: "",
+      })
+    );
+    await waitForResult(rendered);
 
-  expect(rendered.result.current.canValidate).toBeFalsy();
-  expect(rendered.result.current.errors.name).toBeDefined();
-});
+    expect(rendered.result.current.canValidate).toBeFalsy();
+    expect(rendered.result.current.errors.name).toBeDefined();
+  });
 
-it("should be blocked by joi", async () => {
-  const rendered = renderHook(() =>
-    useFormValidation(mockAltSchema, {
-      ...mockFlower,
-      name: "",
-    })
-  );
-  await waitForResult(rendered);
+  it("should be blocked by joi", async () => {
+    const rendered = renderHook(() =>
+      useFormValidation(mockAltSchema, {
+        ...mockFlower,
+        name: "",
+      })
+    );
+    await waitForResult(rendered);
 
-  expect(rendered.result.current.canValidate).toBeFalsy();
-  expect(Object.keys(rendered.result.current.errors)).toHaveLength(1);
-});
+    expect(rendered.result.current.canValidate).toBeFalsy();
+    expect(Object.keys(rendered.result.current.errors)).toHaveLength(1);
+  });
 
-it("should be blocked by a function", async () => {
-  const rendered = renderHook(() =>
-    useFormValidation(mockSchema, {
-      ...mockFlower,
-      petals: -1,
-    })
-  );
-  await waitForResult(rendered);
+  it("should be blocked by a function", async () => {
+    const rendered = renderHook(() =>
+      useFormValidation(mockSchema, {
+        ...mockFlower,
+        petals: -1,
+      })
+    );
+    await waitForResult(rendered);
 
-  expect(rendered.result.current.canValidate).toBeFalsy();
-  expect(rendered.result.current.errors.petals).toBe(MOCK_SCHEMA_PETALS_ERROR);
-});
+    expect(rendered.result.current.canValidate).toBeFalsy();
+    expect(rendered.result.current.errors.petals).toBe(
+      MOCK_SCHEMA_PETALS_ERROR
+    );
+  });
 
-it("should throw a type mismatch error for high level schema", async () => {
-  switchHighLevelValidation("joi");
-  const rendered = renderHook(() => useFormValidation(mockSchema, mockFlower));
-  await rendered.waitForNextUpdate();
+  it("should throw a type mismatch error for high level schema", async () => {
+    switchHighLevelValidation("joi");
+    const rendered = renderHook(() =>
+      useFormValidation(mockSchema, mockFlower)
+    );
+    await rendered.waitForNextUpdate();
 
-  expect(ErrorsKit().lastError).toBe("Schema type mismatch : joi");
+    expect(ErrorsKit().lastError).toBe("Schema type mismatch : joi");
+  });
 });
